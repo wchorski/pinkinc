ddb35f17b3175996d335c2d227ef7cf6daca4cdc
wchorski <wchorski@gmail.com>
2022-08-08 19:41:56 -0500

    fixed function access before initialization

--- components/HeartChart.jsx
+++ components/HeartChart.jsx
@@ -4,7 +4,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-HeartChart.displayName = "HeartChart"
+
 export const HeartChart = forwardRef(({ importData, playerOne }, ref) => {
 
   useImperativeHandle(ref, () => ({
@@ -60,3 +60,4 @@ export const HeartChart = forwardRef(({ importData, playerOne }, ref) => {
     </>
   )
 })
+HeartChart.displayName = "HeartChart"
