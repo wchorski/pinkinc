8558e4384ad95073f8c0024141ce0807219b9db1
wchorski <wchorski@gmail.com>
2022-08-08 19:36:32 -0500

    fixed missing displayName on arrow function

--- components/HeartChart.jsx
+++ components/HeartChart.jsx
@@ -4,7 +4,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-
+HeartChart.displayName = "HeartChart"
 export const HeartChart = forwardRef(({ importData, playerOne }, ref) => {
 
   useImperativeHandle(ref, () => ({
@@ -42,7 +42,7 @@ export const HeartChart = forwardRef(({ importData, playerOne }, ref) => {
   useEffect(() => {
 
     formatData(importData)
-    
+
 
   }, [importData])
 
